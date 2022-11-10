3f5b8105417987ebd6b291e5b785f228594680a5
Demetrio Rafael Briones Briones <83298684+ralpheeet@users.noreply.github.com>
2022-11-09 19:38:14 -0600

    Initital commit

---
+++ test.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+def pussy(dick: int) : -> baby_blue: bool
+    """Function to create baby
+
+    With this function create a baby introducin dick into pussy
+
+    Args:
+        dick: in the real dick, not plastic
+    returns:
+        The baby boolean variable to know if the baby was created
+    """
+
+    baby_blue = True
+
+    return baby_blue
